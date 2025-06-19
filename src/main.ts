971613d25456c1a95a98c2a0f01d9e3babe680ea
Astik Gorai <itsastik2000@gmail.com>
2025-06-19 17:06:28 +0530

    added extra details on cors

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,10 @@ import { JwtService } from '@nestjs/jwt';
 
 async function bootstrap() {
 const app = await NestFactory.create(AppModule);
-app.enableCors();
-
+ app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true, 
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 
